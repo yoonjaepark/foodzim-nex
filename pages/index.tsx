b0360a1d092f7edbf3b0ab14e447e5c16e393c0a
yjpark <yjpark@wemakeprice.com>
2020-08-12 19:01:17 +0900

    antd 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import { Button } from 'antd';
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
+      <Button>dsadsa</Button>
     <h1>Hello Next.js 👋</h1>
     <p>
       <Link href="/about">
