8cc236e26ce8e76999a57cd65d6fd5b45896d450
yjpark <yjpark@wemakeprice.com>
2020-08-13 14:40:00 +0900

    스토어 생성

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import App from 'next/app'
 import '../assets/less/_app.less';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from '../reducers';
+
+const store = createStore(rootReducer);
 
 export default class MyApp extends App {
     render () {
         const { Component, pageProps } = this.props
         return (
-            <Component {...pageProps} />
+            <Provider store={store}>
+                <Component {...pageProps} />
+            </Provider>
         )
     }
 }
