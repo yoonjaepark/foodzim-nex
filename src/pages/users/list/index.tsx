97c6b03b5ba2ad88de1bd781673c8dec9927998b
yjpark <yjpark@wemakeprice.com>
2020-08-18 17:45:54 +0900

    hooks 패턴으로 어레이 추가

--- src/pages/users/list/index.tsx
+++ src/pages/users/list/index.tsx
@@ -1,12 +1,24 @@
 import {GetStaticProps} from 'next'
 import {useSelector} from "react-redux";
 import {RootState} from "../../_app";
+import {useState} from "react";
 
 const WithStaticProps = () => {
     const user = useSelector((state: RootState) => state.user); // store의 state를 불러오는 hook   store의 state 중에서 count의 state를 불러온다.
     console.log(user)
+    const [list, setList] = useState<String[]>([]);
+    const handleAddList = () => {
+        let newList = [...list]
+        newList.push('sss')
+        setList(newList)
+    }
     return (
         <div>
+            <button onClick={handleAddList}>리스트 추가</button>
+            <hr/>
+            {list.map((item, i) => (
+                <div key={i}>{item}</div>
+            ))}
             <h1>사용자 리스트</h1>
             {user.list.map((user, i) => (
                 <div key={i}>
